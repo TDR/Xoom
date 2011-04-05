0cb8c0f07ffd43623cc2b97d83a7ae603bcc6e81
Ohad Ben-Cohen <ohad@wizery.com>
2011-04-05 17:43:20 +0300

    mmc: mmc_card_keep_power cleanups
    mmc_card_is_powered_resumed is a mouthful; instead, simply use
    mmc_card_keep_power, which also better explains the purpose of
    the macro.
    
    Employ mmc_card_keep_power() where possible.
    
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1911,7 +1911,7 @@ int mmc_suspend_host(struct mmc_host *host)
 	}
 	mmc_bus_put(host);
 
-	if (!err && !(host->pm_flags & MMC_PM_KEEP_POWER))
+	if (!err && !mmc_card_keep_power(host))
 		mmc_power_off(host);
 
 	return err;
@@ -1935,7 +1935,7 @@ int mmc_resume_host(struct mmc_host *host)
 	}
 
 	if (host->bus_ops && !host->bus_dead) {
-		if (!(host->pm_flags & MMC_PM_KEEP_POWER)) {
+		if (!mmc_card_keep_power(host)) {
 			mmc_power_up(host);
 			mmc_select_voltage(host, host->ocr);
 			/*
