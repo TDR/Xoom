3c19fb6a92829d2ac6823be4a5ffa36fe69bca6e
TDR <tsdmk01@gmail.com>
2011-05-23 17:59:52 +0000

    Quick fix for incorrect bogomips reported on second core

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -292,6 +292,8 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 	percpu_timer_setup();
 
 	calibrate_delay();
+	struct cpuinfo_arm *cpu0_info = &per_cpu(cpu_data, 0); // Take lpj from CPU0
+	loops_per_jiffy = cpu0_info->loops_per_jiffy;
 
 	smp_store_cpu_info(cpu);
 
